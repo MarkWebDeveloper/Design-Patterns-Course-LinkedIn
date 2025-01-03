3d5a912944ddb393b171260ae185077762fbab55
Mark Kil <markprogramdev@gmail.com>
2025-01-03 12:27:15 +0100

    simulated a duck test situation

--- src/main/java/dev/mark/java_design_patterns/adapter/DuckSimulator.java
+++ src/main/java/dev/mark/java_design_patterns/adapter/DuckSimulator.java
@@ -2,24 +2,13 @@ package dev.mark.java_design_patterns.adapter;
 
 public class DuckSimulator {
     public static void main(String[] args) {
-        MallardDuck duck = new MallardDuck();
         SuperDrone drone = new SuperDrone();
         DroneAdapter droneAdapter = new DroneAdapter(drone);
+        testDuck(droneAdapter);
+    }
 
-        System.out.println("The Duck says...");
-        System.out.println("====================================");
+    static void testDuck(Duck duck) {
         duck.quack();
         duck.fly();
-
-        System.out.println("The Drone says...");
-        System.out.println("====================================");
-        drone.beep();
-        drone.spin_rotors();
-        drone.take_off();
-
-        System.out.println("The DroneAdapter says...");
-        System.out.println("====================================");
-        droneAdapter.quack();
-        droneAdapter.fly();
     }
 }
